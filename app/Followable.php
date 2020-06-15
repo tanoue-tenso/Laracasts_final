01de938d4f19a31172079020db2ca91bbd67e33b
Tanoue Daisuke <tanoue@tenso.com>
2020-06-15 22:38:50 +0900

    クライアント側からフォローの切り替えができるようにする

--- app/Followable.php
+++ app/Followable.php
@@ -11,6 +11,22 @@ trait Followable
         return $this->follows()->save($user);
     }
 
+    // フォローを外す
+    public function unFollow(User $user)
+    {
+        return $this->follows()->detach($user);
+    }
+
+    // ↓↓上２つをまとめた
+    public function toggleFollow(User $user)
+    {
+        if ($this->isFollow($user)) {
+          return $this->unFollow($user);
+        } else {
+           return  $this->follow($user);
+        }
+    }
+
     // 引数のユーザーをフォローしてる？メソッド
     public function isFollow(User $user)
     {

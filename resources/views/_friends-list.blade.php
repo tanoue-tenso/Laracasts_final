6e104d614dcbf5307a27335ca6fbaf777fa9a67c
Tanoue Daisuke <tanoue@tenso.com>
2020-06-16 09:32:06 +0900

    @forelseでtweetやfollowが0のとき用の表示追加

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,12 +1,14 @@
 <h3 class="font-bold text-xl mb-4">Followings</h3>
 
 <ul>
-    @foreach (auth()->user()->follows as $user)
+    @forelse (auth()->user()->follows as $user)
         <li class="mb-4">
             <a href="{{ route('profile', $user) }}" class="flex items-center text-sm">
                 <img src={{ $user->avatar }} alt="user" class="rounded-full mr-2" width="40" height="40">
                 {{ $user->name }}
             </a>
         </li>
-    @endforeach
+    @empty
+        <li>No friends yet ( ˘ω˘ )</li>
+    @endforelse
 </ul>

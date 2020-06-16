e9b62058ebb40d0f3df24a7e3b10c29ff4dda39b
Tanoue Daisuke <tanoue@tenso.com>
2020-06-16 14:27:35 +0900

    likesテーブル追加

---
+++ database/migrations/2020_06_16_051635_create_likes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tweet_id')->constrained()->onDelete('cascade');
+            $table->boolean('liked'); // good or bad
+            $table->timestamps();
+
+            // 上のconstrainedと同じ
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // $table->foreign('tweet_id')->references('id')->on('tweets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
